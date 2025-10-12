2a27495ebc17c2a3bfb5c2d330bc50c692cccdc6
Mihail <93045725+xRoollyx@users.noreply.github.com>
2025-10-12 20:27:37 +0300

    Lesson 3.001

--- Scripts/Game/MainMenu/Root/MainMenuEnterPoint.cs
+++ Scripts/Game/MainMenu/Root/MainMenuEnterPoint.cs
@@ -3,13 +3,13 @@ using myProject.Scripts.BaCon.Scripts;
 using myProject.Scripts.Game.GameRoot;
 using myProject.Scripts.Game.MainMenu.Root.View;
 using UnityEngine;
-using UnityEngine.Serialization;
+
 
 namespace myProject.Scripts.Game.MainMenu.Root{
     public class MainMenuEnterPoint : MonoBehaviour{
         private  DiContainer _mainMenuContainer;
 
-        [SerializeField] private UIMainMenuBinder _sceneUIRootPrefab;
+        [SerializeField] private UIMainMenuRootBinder _sceneUIRootPrefab;
         
         public event Action GoToGameplayButtonClicked;
        
