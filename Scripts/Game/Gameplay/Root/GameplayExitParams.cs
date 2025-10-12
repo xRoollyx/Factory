e281a9b870c7d2e7acdc8c0e13ec00913fd9bafd
Mihail <93045725+xRoollyx@users.noreply.github.com>
2025-10-12 19:46:04 +0300

    Lesson 3 update

---
+++ Scripts/Game/Gameplay/Root/GameplayExitParams.cs
@@ -0,0 +1,9 @@
+﻿namespace myProject.Scripts.Game.Gameplay.Root{
+    public class GameplayExitParams{
+        public MainMenuExitParams MainMenuExitParams{ get; }
+
+        public GameplayExitParams(MainMenuExitParams mainMenuExitParams){
+            MainMenuExitParams = mainMenuExitParams;
+        }
+    }
+}
